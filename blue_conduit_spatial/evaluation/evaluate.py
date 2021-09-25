fee22f9cc02e1b504bbe207ae421ef3602658341
Kevin Hare <kchare8@gmail.com>
2021-09-25 05:17:51 -0400

    Add probability calibration curve and helper function; refactor to use helper function

--- blue_conduit_spatial/evaluation/evaluate.py
+++ blue_conduit_spatial/evaluation/evaluate.py
@@ -1,6 +1,17 @@
 import numpy as np
 from sklearn.metrics import roc_auc_score as ras
 from sklearn.metrics import precision_score, classification_report
+from sklearn.calibration import calibration_curve
+
+def pred2d_to_1d(arr):
+    """Helper method for creating 1D array to enforce
+    same array dimensions for methods"""
+
+    if len(arr.shape) == 1:
+        return arr
+    else:
+        return arr[:,1]
+
 
 def roc_auc_score(y_true, y_pred):
     """Returns the ROC-AUC Score for some y_true and y_test. Can pass predicted
@@ -14,14 +25,8 @@ def roc_auc_score(y_true, y_pred):
     Returns:
         ROC-AUC score for the metrics
     """
-    try:
-        if y_pred.shape[1] > 2:
-            raise ValueError('Only two classes can be passed')
-        else:
-            return ras(y_true, y_pred[:,1])
-    except:
-        return ras(y_true, y_pred)
-
+    yhat = pred2d_to_1d(y_pred)
+    return ras(y_true, y_pred[:,1])
 
 def hit_rate(y_true, y_pred, threshold=0.5):
     """Returns the 'hit rate'. This is the total predicted positive that actually
@@ -62,10 +67,7 @@ def generate_hit_rate_curve(y_true, y_pred):
         pred_probs: Predicted probabilities for ordered test set
     """
     # Handle passing of 1-D and 2-D arrays
-    if len(y_pred.shape) == 1:
-        yhat = y_pred
-    else:
-        yhat = y_pred[:,1]
+    yhat = pred2d_to_1d(y_pred)
 
     y_comb = np.stack([y_true, yhat], axis=1)
 
@@ -77,13 +79,29 @@ def generate_hit_rate_curve(y_true, y_pred):
     hit_rates = np.cumsum(y_comb_sorted[:,0])/(np.arange(y_comb_sorted.shape[0]) + 1)
     return hit_rates, y_comb_sorted[:,1]
 
+def generate_calibration_curve(y_true, y_pred, n_bins=10, **kwargs):
+    """Mask (with error handling) for sk-learn calibration curve
+
+    Args:
+        y_true: Ground truth labels. Should be binary.
+        y_pred: Prediction probabilities or class labels
+        n_bins: number of bins to discretize
+    
+    Returns:
+        true_curve: Bucket mean true positives
+        pred_curve: Bucket mean predicted positives
+    """
+    yhat = pred2d_to_1d(y_pred)
+
+    return calibration_curve(y_true, yhat, n_bins=n_bins, **kwargs)
+
 
 if __name__ == '__main__':
     raw_data = np.load('../../data/predictions/baseline_preds.npz', allow_pickle=True)
     y_true = raw_data['ytrue'].astype(int)
     y_hat = raw_data['yhat'].astype(float)
 
-    ra_sk = generate_hit_rate_curve(y_true, y_hat)
+    ra_sk = generate_calibration_curve(y_true, y_hat)
     print(ra_sk)
 
     
