1692fcfa806ea886acdf290eb9c034c613cc1c3d
Kevin Hare <kchare8@gmail.com>
2021-10-04 14:03:27 -0400

    Basics for OSRM data

--- blue_conduit_spatial/osrm_setup/orsm_postrestart_setup.sh
+++ blue_conduit_spatial/osrm_setup/orsm_postrestart_setup.sh
@@ -1,3 +1,9 @@
+sudo /bin/dd if=/dev/zero of=/var/swapfile bs=1M count=10240
+sudo /sbin/mkswap /var/swapfile
+sudo chmod 600 /var/swapfile
+sudo /sbin/swapon /var/swapfile
+
+
 wget http://download.geofabrik.de/north-america/us/michigan-latest.osm.pbf
 
 docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/michigan-latest.osm.pbf
--- blue_conduit_spatial/osrm_setup/orsm_prerestart_setup.sh
+++ blue_conduit_spatial/osrm_setup/orsm_prerestart_setup.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
+
 sudo apt update
-sudo apt-install docker.io
+sudo apt install docker.io
 #sudo apt install -y git cmake build-essential jq htop
 sudo service docker start
 sudo usermod -a -G docker ubuntu
